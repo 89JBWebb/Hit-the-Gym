0b2d3427544e290cd9639cba1d9774e75eb9f202
JWebb22 <89JBWebb@gmail.com>
2023-04-05 16:32:48 -0400

    made a game

--- Taxi.py
+++ Taxi.py
@@ -1,5 +1,21 @@
 import gym
 
+acts = {
+    "w": 1,
+    "s": 0,
+    "a": 3,
+    "d": 2,
+    "q": 4,
+    "e": 5
+}
+
 env = gym.make("Taxi-v3", render_mode="ansi")
-env.reset()
+state = env.reset()
+
 print(env.render())
+imp = input("Action:")
+
+while imp != "exit":
+    env.step(acts[imp])
+    print(env.render())
+    imp = input("Action:")
